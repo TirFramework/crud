206cd88285257b3cbce5835326fa3c0ea2ac05cc
Tirdad <abbasi.tirdad@gmail.com>
2020-05-25 21:24:50 +0430

    menu item and page bug fixed

--- src/Resources/Views/scaffold/inputTypes/select.blade.php
+++ src/Resources/Views/scaffold/inputTypes/select.blade.php
@@ -10,42 +10,34 @@ else:
     $options ['placeholder'] = trans('crud::panel.select').' '.trans("$crud->name::panel.$field->display");
 endif;
 
-
-$model = null;
-if( isset($item->{$field->name}) ){
-$model = $item->{$field->name};
-}
 ?>
 
-<div class="{{$field->col ?? 'col-12 col-md-12'}}">
+<div class="{{  $field->col ?? 'col-12 col-md-12' }}">
     <div class="form-group">
-        {{-- {!! Form::label($fieldName,trans("$crud->name::panel.$field->display"), ['class' => ' control-label']) !!}
-        <div class="">
             {!! Form::select($fieldName, $field->data, null,$options)!!}
-        </div> --}}
 
 
-        <select class="form-control
-             @error( $field->name ) is-invalid @enderror"
-                id="{{$field->name}}"
-                name="{{$field->name}}"
-            >
+{{--        <select class="form-control--}}
+{{--             @error( $field->name ) is-invalid @enderror"--}}
+{{--                id="{{$field->name}}"--}}
+{{--                name="{{$field->name}}"--}}
+{{--            >--}}
 
-            @isset( $field->placeholder )
-                <option selected="selected" value="">
-                    {{$field->placeholder}}
-                </option>
-            @endisset
+{{--            @isset( $field->placeholder )--}}
+{{--                <option selected="selected" value="">--}}
+{{--                    {{$field->placeholder}}--}}
+{{--                </option>--}}
+{{--            @endisset--}}
 
-            @foreach ($field->data as $key => $option)
-            <option
-            @if(
-            old($field->name, $model ) == $key
-            ) {{ 'selected' }}
-                @endif
-                value="{{$key}}">{{$option}}</option>
-            @endforeach
-        </select>
+{{--            @foreach ($field->data as $key => $option)--}}
+{{--            <option--}}
+{{--            @if(--}}
+{{--            old($field->name, $model ) == $key--}}
+{{--            ) {{ 'selected' }}--}}
+{{--                @endif--}}
+{{--                value="{{$key}}">{{$option}}</option>--}}
+{{--            @endforeach--}}
+{{--        </select>--}}
 
         <label for="{{$field->name}}" class="control-label text-right">@lang("$crud->name::panel.$field->display")</label>
     </div>
