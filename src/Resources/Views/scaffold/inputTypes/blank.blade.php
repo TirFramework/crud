72c7cef2999eb146d6ad61157b0abfd5df7311b4
Tirdad <abbasi.tirdad@gmail.com>
2020-05-26 09:45:30 +0430

    frontend development and debugging

--- src/Resources/Views/scaffold/inputTypes/blank.blade.php
+++ src/Resources/Views/scaffold/inputTypes/blank.blade.php
@@ -1,5 +1,7 @@
-<div class="form-group">
-    <div class="col-md-12">
+
+
+<div class="{{$field->col ?? 'col-12 col-md-12'}}">
+    <div class="form-group">
         {!! $field->value !!}
     </div>
 </div>
