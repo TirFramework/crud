dfdb05d022aa2e4e7ee094725878ec0ce023d747
Tirdad <abbasi.tirdad@gmail.com>
2020-06-04 09:08:36 +0430

    coupon tax shipping and payment add to project

---
+++ src/Support/Services/Manager.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tir\Crud\Support\Services;
+
+use Illuminate\Support\Arr;
+
+abstract class Manager
+{
+    private $drivers = [];
+
+    public function all()
+    {
+        return collect($this->drivers);
+    }
+
+    public function names()
+    {
+        return array_keys($this->drivers);
+    }
+
+    public function get($name)
+    {
+        return Arr::get($this->drivers, $name);
+    }
+
+    public function register($name, $driver)
+    {
+        $this->drivers[$name] = is_callable($driver) ? call_user_func($driver) : $driver;
+
+        return $this;
+    }
+
+    public function count()
+    {
+        return count($this->drivers);
+    }
+
+    public function isEmpty()
+    {
+        return empty($this->drivers);
+    }
+
+    public function isNotEmpty()
+    {
+        return ! $this->isEmpty();
+    }
+}
