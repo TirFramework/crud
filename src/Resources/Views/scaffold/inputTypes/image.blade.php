b8a2a7a182dc9c20de3f496adf8b232068d51259
Tirdad <abbasi.tirdad@gmail.com>
2020-07-21 16:02:28 +0430

    Blog Development

--- src/Resources/Views/scaffold/inputTypes/image.blade.php
+++ src/Resources/Views/scaffold/inputTypes/image.blade.php
@@ -3,25 +3,29 @@
 $name = $field->name;
 
 // remove '[' and ']' from field name because fix upload problem
-$index = preg_replace('/[\[\]]/i', '_' , $field->name);
+$clearName = preg_replace('/[\[\]]/i', '_' , $field->name);
+$index =
 @endphp
 
 <div class="{{$field->col ?? 'col-12 col-md-12'}}">
     <div class="form-group">
         <div class="input-group">
             <span class="input-group-btn">
-                <a id="{{$index}}" data-input="{{$index}}_input" data-preview="{{$index}}_holder" class="image-btn btn btn-primary">
+                <a id="{{$clearName}}" data-input="{{$clearName}}_input" data-preview="{{$clearName}}_holder" class="image-btn btn btn-primary">
                     <i class="fas fa-image"></i> {{trans('crud::panel.choose')}}
                 </a>
             </span>
-            {!! Form::text($field->name,null,['class' => 'form-control','id'=>$index.'_input', 'placeholder'=> trans("$crud->name::panel.$field->name")])!!}
+            {!! Form::text($field->name,null,['class' => 'form-control','id'=>$clearName.'_input', 'placeholder'=> trans("$crud->name::panel.$field->name")])!!}
         </div>
         {!! Form::label("$field->name", trans("$crud->name::panel.$field->display"), ['class' => 'control-label uploder-label']) !!}
-{{--        <img id="{{$index}}_holder" @isset($image) src="{{url('/').'/'.$image}}" @endisset class="image-holder">--}}
+{{--        <img id="{{$clearName}}_holder" @isset($image) src="{{url('/').'/'.$image}}" @endisset class="image-holder">--}}
 
-        <div class="image-holder" id="{{$index}}_holder">
+        <div class="image-holder" id="{{$clearName}}_holder">
             @isset($item->{$name})
+
                 <img src="{{ $item->{$name} }}" alt="" style="max-width: 100px; max-height: 100px">
+
+
             @endisset
         </div>
 
@@ -31,6 +35,6 @@ $index = preg_replace('/[\[\]]/i', '_' , $field->name);
 
 @push('scripts')
     <script>
-        $('#{{$index}}').filemanager('image');   //btn image
+        $('#{{$clearName}}').filemanager('image');   //btn image
     </script>
 @endpush
