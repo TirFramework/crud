590e106cbd85e2522b29fe4f568d70e2a336b589
ta <unset>
2021-10-09 19:33:25 +0330

    add delete functunality

--- src/Controllers/CrudController.php
+++ src/Controllers/CrudController.php
@@ -6,7 +6,7 @@ use Illuminate\Routing\Controller as BaseController;
 
 abstract class CrudController extends BaseController
 {
-    use IndexTrait, DataTrait, CreateTrait, StoreTrait, EditTrait, UpdateTrait, ValidationTrait, SelectTrait;
+    use IndexTrait, DataTrait, CreateTrait, StoreTrait, EditTrait, UpdateTrait, ValidationTrait, SelectTrait, DestroyTrait;
 
     protected abstract function setModel(): string;
 
