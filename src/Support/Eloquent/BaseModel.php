9a2b57c8ee7427fc20ff4a2c5940af0c22ee844d
ta <unset>
2021-10-09 19:51:10 +0330

    add soft delete to model

--- src/Support/Eloquent/BaseModel.php
+++ src/Support/Eloquent/BaseModel.php
@@ -5,11 +5,14 @@ namespace Tir\Crud\Support\Eloquent;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Tir\Crud\Support\Scaffold\BaseScaffold;
 
 abstract class BaseModel extends Model
 {
     use BaseScaffold;
+
+    use SoftDeletes;
 
     protected $dates = ['deleted_at'];
 
