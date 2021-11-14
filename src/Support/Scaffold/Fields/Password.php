39428fe75e94bc95eaa950fbf216eaf56b07e7fe
mehrdad <javadabbasi72@gmail.com>
2021-11-14 21:44:21 +0330

    add password

---
+++ src/Support/Scaffold/Fields/Password.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tir\Crud\Support\Scaffold\Fields;
+
+ class Password extends BaseField
+{
+
+     protected string $type = 'Password';
+
+     public static function select(string $string)
+     {
+     }
+
+ }
