8ef7b9dcbe0ac4984e6b020ba1a84a7f71cca3af
mehrdad <javadabbasi72@gmail.com>
2023-03-30 14:54:31 +0430

    add editor

--- src/Support/Scaffold/Fields/Editor.php
+++ src/Support/Scaffold/Fields/Editor.php
@@ -2,17 +2,30 @@
 
 namespace Tir\Crud\Support\Scaffold\Fields;
 
- class Editor extends BaseField
+class Editor extends BaseField
 {
 
-     protected string $type = 'Editor';
-     protected int $height = 400;
+   protected string $type = 'Editor';
+   protected string $postUrl = '';
+   protected string $basePath = '';
+   protected int $height = 400;
 
 
-     public function height(int $value): BaseField
-     {
-        $this->height = $value;
-        return $this;
-     }
+   public function height(int $value): BaseField
+   {
+      $this->height = $value;
+      return $this;
+   }
 
- }
+   public function basePath(string $value): BaseField
+   {
+      $this->basePath = $value;
+      return $this;
+   }
+
+   public function postUrl(string $value): BaseField
+   {
+      $this->postUrl = $value;
+      return $this;
+   }
+}
