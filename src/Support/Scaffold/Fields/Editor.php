f038cc9b6133feff7b0bed92dc5a4619110ee544
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2023-03-30 17:55:41 +0200

    update editor

--- src/Support/Scaffold/Fields/Editor.php
+++ src/Support/Scaffold/Fields/Editor.php
@@ -2,17 +2,30 @@
 
 namespace Tir\Crud\Support\Scaffold\Fields;
 
- class Editor extends BaseField
+class Editor extends BaseField
 {
 
-     protected string $type = 'Editor';
-     protected int $height = 400;
+   protected string $type = 'Editor';
+   protected string $uploadUrl;
+   protected string $basePath = '/storage/';
+   protected int $height = 400;
 
 
-     public function height(int $value): BaseField
-     {
-        $this->height = $value;
-        return $this;
-     }
+    public function height(int $value): Editor
+   {
+      $this->height = $value;
+      return $this;
+   }
 
- }
+   public function basePath(string $value): Editor
+   {
+      $this->basePath = $value;
+      return $this;
+   }
+
+   public function uploadUrl(string $value): Editor
+   {
+      $this->uploadUrl = $value;
+      return $this;
+   }
+}
